e8b73c6e22d93e7553a0f1dfc7a9191c381af47c
Arnau <4bvives@gmail.com>
2024-03-14 19:04:11 +0100

    Tab layout

---
+++ src/components/TabsMenu.vue
@@ -0,0 +1,16 @@
+<template>
+   <ion-page>
+      <ion-tabs>
+         <ion-router-outlet></ion-router-outlet>
+         <ion-tab-bar slot="bottom">
+            <ion-tab-button tab="home" href="/home">Home</ion-tab-button>
+            <ion-tab-button tab="library" href="/library">Library</ion-tab-button>
+            <ion-tab-button tab="search" href="/search">Search</ion-tab-button>
+            <ion-tab-button tab="radio" href="/radio">Radio</ion-tab-button>
+         </ion-tab-bar>
+      </ion-tabs>
+   </ion-page>
+</template>
+<script setup lang="ts">
+   import {IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton} from '@ionic/vue'
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,35 @@
+import TabsMenu from '@/components/TabsMenu.vue';
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '',
-    redirect: '/folder/Inbox'
+    redirect: '/home'
   },
   {
-    path: '/folder/:id',
-    component: () => import ('../views/FolderPage.vue')
-  }
+    path:'',
+    component:TabsMenu,
+    children:[
+      {
+        path: '/home',
+        component: () => import ('../views/Home.vue')
+      },
+      {
+        path: '/library',
+        component: () => import ('../views/Library.vue')
+      },
+      {
+        path: '/radio',
+        component: () => import ('../views/Radio.vue')
+      },
+      {
+        path: '/search',
+        component: () => import ('../views/Search.vue')
+      },
+    ]
+  },
+  
 ]
 
 const router = createRouter({
